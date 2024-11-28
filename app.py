526309971864073808e611fce989276e8f38c38a
Abid <abid.ranabegum@gmail.com>
2024-11-28 23:39:37 +0530

    added bar graph

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from datetime import date
 from datetime import datetime
 import random
 import pandas as pd 
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///MyExpenses.db"
@@ -21,29 +22,24 @@ class Expenses(db.Model):
 
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
-    if request.method == "POST":
-        exp = Expenses(
-            sno = int(random.random()*10000),
-            date= datetime.strptime(request.form.get("date"),'%Y-%m-%d') ,
-            category=request.form.get("category"),
-            amount=request.form.get("amount")
-        )
-        db.session.add(exp)
-        db.session.commit()
-        print(exp)
-        items = Expenses()
-        expense_items = items.query.all()
-        return render_template('index.html',myTable = expense_items)
-    
     items = Expenses()
     expense_items = items.query.all()
     list_form = []
+    list_form2 = []
     for d in expense_items:
         list_form.append({'category':d.category,'amount':d.amount})
+        list_form2.append({'date':d.date,'amount':d.amount})
     pandas_df = pd.DataFrame(list_form)
+    pandas_df2 = pd.DataFrame(list_form2)
     if len(pandas_df)>0:
         grouped_df = pandas_df.groupby('category')['amount'].sum().reset_index()
-        return render_template('index.html',myTable = expense_items , data=grouped_df.to_json(orient='records', index=False))
+        grouped_df2 = pandas_df2.groupby('date')['amount'].sum().reset_index()
+        print(grouped_df2.dtypes['date'])
+        grouped_df2['date'] = grouped_df2['date'].apply(lambda x:x.date())
+        print(grouped_df2.dtypes['date'])
+        print(grouped_df2)
+        return render_template('index.html',myTable = expense_items , data=grouped_df.to_json(orient='records', index=False) ,
+                               bar_graph_data = grouped_df2.to_json(orient='records', index=False))
     return render_template('index.html',myTable = expense_items)
 
 
